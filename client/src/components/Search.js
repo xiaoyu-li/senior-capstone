3bbef05f1424281ae2d94b70bb01c86851112e1c
Michael <lxyamerica@gmail.com>
2017-11-01 23:37:05 -0500

    add top search bar

---
+++ client/src/components/Search.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchIcon from 'material-ui-icons/Search';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  wrapper: {
+    height: 40,
+    fontFamily: theme.typography.fontFamily,
+    position: 'relative',
+    borderRadius: 4,
+    background: '#f5f5f5',
+    '& $input': {
+      transition: 'background 100ms ease-in',
+      width: 500,
+      '&:focus': {
+        border: '1px solid rgba(0,0,0,0.12)',
+        borderRadius: 4,
+        boxShawdow: '0 1px 1px rgba(0,0,0,0.24)',
+        background: theme.palette.common.fullWhite,
+      },
+    },
+  },
+  icon: {
+    color: theme.palette.common.lightBlack,
+  },
+  search: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    font: 'inherit',
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit}px ${theme.spacing.unit}px ${theme
+      .spacing.unit * 9}px`,
+    border: 0,
+    display: 'block',
+    verticalAlign: 'middle',
+    whiteSpace: 'normal',
+    background: 'none',
+    margin: 0, // Reset for Safari
+    width: '100%',
+    '&:focus': {
+      outline: 0,
+    },
+  },
+});
+
+const Search = (props) => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.search}>
+        <SearchIcon className={classes.icon} />
+      </div>
+      <input id="docsearch-input" placeholder="Search your goal" className={classes.input} />
+    </div>
+  );
+};
+
+Search.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Search);
