d13f0df7048e3a14aa8060eef90a6eeab083752f
Michael <lxyamerica@gmail.com>
2017-11-03 01:55:50 -0500

    add chakras data

---
+++ client/src/common/chakras.js
@@ -0,0 +1,46 @@
+const chakras = [
+  {
+    name: 'Muladhara',
+    type: 'Root',
+    progress: '10',
+    meaning: ' Balance of physical body, and clearing of fear',
+  },
+  {
+    name: 'Swadhisthana',
+    type: 'Sacral',
+    progress: '10',
+    meaning: 'Relief of tension, augment relationships',
+  },
+  {
+    name: 'Manipura',
+    type: 'Solar',
+    progress: '10',
+    meaning: 'Clearing of anger, augment joy and self confidence',
+  },
+  {
+    name: 'Anahata',
+    type: 'Heart',
+    progress: '10',
+    meaning: 'Love, harmony, connection and peace',
+  },
+  {
+    name: 'Vishuddha',
+    type: 'Throat',
+    progress: '10',
+    meaning: 'Communication and self expression',
+  },
+  {
+    name: 'Ajna',
+    type: 'Third Eye',
+    progress: '10',
+    meaning: 'Spiritual connectedness, insight, and visualization',
+  },
+  {
+    name: 'Sahasrara',
+    type: 'Crown',
+    progress: '10',
+    meaning: 'Perception beyond the confines of space and time. At one with everything',
+  },
+];
+
+export default chakras;
