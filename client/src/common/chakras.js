b3fd19e46e757d12e16f7b267f10991277ef1a8d
Michael <lxyamerica@gmail.com>
2017-11-03 20:00:40 -0500

    update data files

--- client/src/common/chakras.js
+++ client/src/common/chakras.js
@@ -1,41 +1,48 @@
 const chakras = [
   {
+    id: 1,
     name: 'Muladhara',
     type: 'Root',
     progress: '10',
     meaning: ' Balance of physical body, and clearing of fear',
   },
   {
+    id: 2,
     name: 'Swadhisthana',
     type: 'Sacral',
     progress: '10',
     meaning: 'Relief of tension, augment relationships',
   },
   {
+    id: 3,
     name: 'Manipura',
     type: 'Solar',
     progress: '10',
     meaning: 'Clearing of anger, augment joy and self confidence',
   },
   {
+    id: 4,
     name: 'Anahata',
     type: 'Heart',
     progress: '10',
     meaning: 'Love, harmony, connection and peace',
   },
   {
+    id: 5,
     name: 'Vishuddha',
     type: 'Throat',
     progress: '10',
     meaning: 'Communication and self expression',
   },
   {
+    id: 6,
     name: 'Ajna',
     type: 'Third Eye',
     progress: '10',
     meaning: 'Spiritual connectedness, insight, and visualization',
   },
   {
+    id: 7,
     name: 'Sahasrara',
     type: 'Crown',
     progress: '10',
---
+++ client/src/common/goals.js
@@ -0,0 +1,9 @@
+const goals = [
+  {
+    id: 100,
+    goals: ['1', '2', '3', '4', '5'],
+    chakrasId: 1,
+  },
+];
+
+export default goals;
